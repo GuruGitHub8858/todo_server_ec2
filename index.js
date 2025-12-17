388edb684b28ad7d7f3f9ab1d05299a8e3ee11cc
GuruGitHub8858 <dr.guru750@gmail.com>
2025-12-17 19:14:34 +0530

    bye

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 connectDb();
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"]
-}));
-
-app.options("*", cors());
+app.use(cors())
 
 app.use(express.json())
 
