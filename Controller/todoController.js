c30ddb7e982f4af15be0e2d007b89f0c884a56b1
GuruGitHub8858 <dr.guru750@gmail.com>
2025-12-17 17:46:02 +0530

    db

--- Controller/todoController.js
+++ Controller/todoController.js
@@ -3,14 +3,31 @@ import todoCollection from "../Model/todoModel.js";
 // store my todo    req.body  hello luffy
 export const addTodo = async (req, res) => {
     try {
-        const data = new todoCollection(req.body);
-        await data.save()
-        res.status(201).json({ mess: "data has been stored" })
-    } catch (err) {
-        res.status(400).json({ message: err })
-    }
+        const { todo } = req.body;
 
-}
+        // validation
+        if (!todo || typeof todo !== "string") {
+            return res.status(400).json({
+                message: "todo must be a non-empty string"
+            });
+        }
+
+        const data = new todoCollection({
+            todo: todo.trim()
+        });
+
+        await data.save();
+
+        res.status(201).json({
+            message: "data has been stored",
+            data
+        });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
 
 export const getTodo = async (req, res) => {
     try {
